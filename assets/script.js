23f89ea8005716d5ea057f4c09b21eca7f44f5b3
unknown <lawrencecaj@gmail.com>
2025-04-29 16:31:43 -0400

    fixed event listener

--- assets/script.js
+++ assets/script.js
@@ -2,11 +2,11 @@ const apiKey = '1d342ae9b0b6856504e9dad6fe843946';
     const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=imperial';
 
     const searchBox = document.querySelector('.search input');
-    const searchBtn = document.querySelector('.search Btn');
+    const searchBtn = document.querySelector('.search button');
 
 
     async function checkWeather(city) {
-        const response = await fetch(`${apiUrl}&q=city&appid=${apiKey}`);
+        const response = await fetch(`${apiUrl}&q=${city}&appid=${apiKey}`);
         var data = await response.json();
         console.log(data);
 
